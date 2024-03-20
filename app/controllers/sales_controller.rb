8a63e080b5d4e353514d8ca62118ea694797e575
Shakhboz <shfazliddinov1940@gmail.com>
2024-03-20 14:43:20 +0500

    m

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -78,6 +78,10 @@ class SalesController < ApplicationController
     buyer = Buyer.find(params[:buyer_id])
     last_one = buyer.sales.order(created_at: :asc).last
     if !last_one.nil? && !last_one.closed?
+      if last_one.product_sells.empty?
+        last_one.update(created_at: DateTime.current)
+      end
+
       redirect_to sale_url(last_one), notice: "Теперь добавьте продажу товаров"
     else
       sfs = Sale.new(buyer: buyer, user: current_user)
