af89c4b4e2988b027150f71d159ccd0291f0f5dd
Shakhboz <shfazliddinov1940@gmail.com>
2024-03-24 11:41:42 +0500

    minor change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,44 +3,6 @@ class PagesController < ApplicationController
     # TODO include discount when calculating a remaining balance
     @buyers = Buyer.all.order(active: :desc)
     @providers = Provider.all.order(active: :desc)
-    @delivery_from_counterparties =
-      DeliveryFromCounterparty.where('created_at >= ?', DateTime.current.beginning_of_day)
-                              .or(DeliveryFromCounterparty.unpaid)
-                              .order(created_at: :desc)
-    @expenditures = Expenditure.where('created_at >= ?', DateTime.current.beginning_of_day)
-                               .or(Expenditure.unpaid).order(created_at: :desc)
-    @sales = Sale.where('created_at >= ?', DateTime.current.beginning_of_day)
-                 .or(Sale.unpaid).order(created_at: :desc)
-
-    unless params.dig(:q, :created_at_end_of_day_lteq)
-      params[:q] ||= {}
-      params[:q][:created_at_end_of_day_lteq] = DateTime.current.end_of_day
-    end
-    unless params.dig(:q, :created_at_gteq)
-      params[:q] ||= {}
-      params[:q][:created_at_gteq] = DateTime.current.beginning_of_day
-    end
-
-    @q = TransactionHistory.ransack(params[:q])
-    sales = Sale.where('created_at >= ?', params[:q][:created_at_gteq])
-               .where('created_at <= ?', params[:q][:created_at_end_of_day_lteq])
-    @sales_in_usd = sales.price_in_usd
-    @sales_in_uzs = sales.price_in_uzs
-
-    delivery_from_counterparties =
-      DeliveryFromCounterparty.where('created_at >= ?', params[:q][:created_at_gteq])
-                              .where('created_at <= ?', params[:q][:created_at_end_of_day_lteq])
-    @delivery_from_counterparties_in_usd = delivery_from_counterparties.price_in_usd
-    @delivery_from_counterparties_in_uzs = delivery_from_counterparties.price_in_uzs
-
-    expenditures =
-      Expenditure.where('created_at >= ?', params[:q][:created_at_gteq])
-                 .where('created_at <= ?', params[:q][:created_at_end_of_day_lteq])
-    @expenditures_in_usd = expenditures.price_in_usd
-    @expenditures_in_uzs = expenditures.price_in_uzs
-
-    @total_salary = Salary.where('created_at >= ?', params[:q][:created_at_gteq])
-                          .where('created_at <= ?', params[:q][:created_at_end_of_day_lteq])
   end
 
   def define_sale_destination; end
