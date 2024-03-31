f96d0e46ffe6b2b4ea221cb2d0d1c89347f66b41
Shakhboz <shfazliddinov1940@gmail.com>
2024-03-31 21:24:17 +0500

    initial setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require bootstrap
+//= require initial_config
 //= require_tree .
--- app/assets/javascripts/initial_config.js
+++ app/assets/javascripts/initial_config.js
@@ -3,4 +3,21 @@ $(document).on('turbo:load', function() {
     $(this).select();
   });
   $(function(){$(".datepicker").datepicker({dateFormat: 'dd-mm-yy'})});
+  $('#searchInput').focus()
+  document.getElementById('searchInput').addEventListener('input', function() {
+    var input, filter, table, tr, nameAttr, i;
+    input = this.value.toUpperCase();
+    table = document.getElementById('buyers-list');
+    tr = table.getElementsByTagName('tr');
+    for (i = 0; i < tr.length; i++) {
+      nameAttr = tr[i].getAttribute('data-buyer-name');
+      if (nameAttr) {
+        if (nameAttr.toUpperCase().startsWith(input)) {
+          tr[i].style.display = '';
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  });
 });
