3faa86f910fb440b37914af7da02248efb53e357
Shakhboz <shfazliddinov1940@gmail.com>
2024-04-14 21:41:23 +0500

    added host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
-  config.hosts << "sam-nerj-production.up.railway.app"
+  config.hosts << ENV.fetch('APP_HOST_URL')
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
