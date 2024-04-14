5003cd05492bd451f38e4db1646f06a2f71b912d
Shakhboz <shfazliddinov1940@gmail.com>
2024-04-14 17:11:03 +0500

    m

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,8 +8,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "sam-nerj-production.up.railway.app"
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
+  config.hosts << "sam-nerj-production.up.railway.app"
   config.cache_classes = true
 
   # Eager loading loads your whole application. When running a single test locally,
