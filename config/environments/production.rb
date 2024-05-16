1a65468fcba2926ffa800153e8050bd0da58ff5b
Shakhboz <shfazliddinov1940@gmail.com>
2024-05-16 15:38:39 +0500

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.active_storage.service = :google
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -37,7 +38,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
