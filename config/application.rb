565c69d5c65284d712984fd841a8e38ae67de25c
Shakhboz <shfazliddinov1940@gmail.com>
2024-05-21 16:29:29 +0500

    made dynamic language

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module PaperApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = ENV.fetch('DEFAULT_LANGUAGE').to_sym
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
