5409c97f086107094102aaac2e68ccdf0c92de6f
Shakhboz <shfazliddinov1940@gmail.com>
2024-05-29 15:47:26 +0500

    m

---
+++ db/migrate/20240529093439_add_buyer_context_to_users.rb
@@ -0,0 +1,5 @@
+class AddBuyerContextToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :buyer_context, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_28_110259) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_29_093439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -483,6 +483,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_28_110259) do
     t.string "telegram_chat_id"
     t.string "string"
     t.string "context"
+    t.integer "buyer_context"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
