ffab6cbcaba88e5efcef93efb7065ddea988af7d
Shakhboz <shfazliddinov1940@gmail.com>
2024-06-02 21:37:11 +0500

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.web_console.whitelisted_ips = '185.213.229.172'
+  config.web_console.permissions = '192.168.0.0/16'
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.asset_host = ENV.fetch('HOST_URL')
 
