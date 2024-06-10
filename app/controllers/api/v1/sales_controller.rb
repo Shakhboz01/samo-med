6393460f6c55248578806f4efe6cf20593e7ec70
Shakhboz <shfazliddinov1940@gmail.com>
2024-06-10 18:40:44 +0500

    m

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class SalesController < Api1Controller
+      skip_before_action :verify_authenticity_token
+
       def show
         @sale = Sale.find(params[:id])
         @product_sells = @sale.product_sells
