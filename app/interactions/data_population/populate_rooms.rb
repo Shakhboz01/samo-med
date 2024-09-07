38ca0cc91a6313339cad6b9554307fe0cbafe6f0
Shakhboz <shfazliddinov1940@gmail.com>
2024-09-07 09:56:18 +0500

    m

--- app/interactions/data_population/populate_rooms.rb
+++ app/interactions/data_population/populate_rooms.rb
@@ -1,11 +1,11 @@
 module DataPopulation
-  class PopulateProducts < ActiveInteraction::Base
+  class PopulateRooms < ActiveInteraction::Base
     def execute
       rooms = JSON.parse(File.read('app/assets/javascripts/rooms.json'))
 
       rooms.each do |room|
         Room.create(
-          name: room['XONA_NOMI_RAQAMI']
+          name: room['XONA_NOMI_RAQAMI'],
           capacity: room['NECHI_KISHILIK']
         )
       end
