d21e1b72b397a1a1ada7f4abbb0b504250b84ef7
Shakhboz <shfazliddinov1940@gmail.com>
2024-10-27 09:37:14 +0500

    m

---
+++ app/jobs/send_request_to_printer.rb
@@ -0,0 +1,23 @@
+# app/jobs/send_request_to_printer.rb
+class SendRequestToPrinter < ApplicationJob
+  queue_as :default
+
+  def perform(sale_id)
+
+    # Use HTTParty to send a request to the printer server
+    response = HTTParty.post("http://localhost:4000/print/#{sale_id}",
+      body: {
+        id: sale_id,
+        # Add any additional data needed for the print request here
+      }.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    );
+
+    # Log or handle response if necessary
+    # if response.success?
+    #   Rails.logger.info("Print request successful for sale ##{sale_id}")
+    # else
+    #   Rails.logger.error("Print request failed for sale ##{sale_id}: #{response.body}")
+    # end
+  end
+end
