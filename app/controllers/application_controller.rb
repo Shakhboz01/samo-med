a5bc0431b3926d0fdf0f8a7cca89b8442173fefd
Shakhboz <shfazliddinov1940@gmail.com>
2024-11-25 22:27:59 +0500

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   before_action :authenticate_user!
-  before_action :check_expiry_date
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -13,12 +12,6 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
-  def check_expiry_date
-    expiry_date = Date.new(2024, 11, 15)
-    if Date.today > expiry_date
-      render plain: 'Sizning tarif rejangiz tugadi.', status: :forbidden
-    end
-  end
   protected
 
   def configure_permitted_parameters
